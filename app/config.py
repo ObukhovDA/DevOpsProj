2f492e782d928bc4b699d69aea07aca0027da24d
ObukhovDA <douxov@mail.ru>
2024-02-29 19:48:45 +0300

    auges

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 import os
 from os import getenv
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = '123'
 
-SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{getenv("MYSQL_USER")}:{getenv("MYSQL_PASSWORD")}@{getenv("MYSQL_HOST")}:3306/{getenv("MYSQL_DATABASE")}'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://test:test@localhost:3306/test'
+# SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{getenv("MYSQL_USER")}:{getenv("MYSQL_PASSWORD")}@{getenv("MYSQL_HOST")}:3306/{getenv("MYSQL_DATABASE")}'
 # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2222_devops:123456789@std-mysql.ist.mospolytech.ru/std_2222_devops'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
