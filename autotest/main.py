7c764fd522c5415593689ab2bf9952c6b9b7fe88
ObukhovDA <douxov@mail.ru>
2024-02-29 18:27:34 +0300

    au

---
+++ autotest/main.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time, sys
+from selenium.webdriver.common.by import By
+
+from auth import auth
+from comments import comments
+from items import items
+
+BASE_ADDRESS = 'http://127.0.0.1:5000/'
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+driver.maximize_window()
+driver.implicitly_wait(60)
+   
+driver.get(BASE_ADDRESS)
+
+
+result_auth  = auth(driver=driver, address=BASE_ADDRESS)
+assert result_auth == True, "Ошибка при авторизации"
+
+result_items  = items(driver=driver, address=BASE_ADDRESS)
+assert result_items == True, "Ошибка при добавлении предмета"
+
+result_comments  = comments(driver=driver, address=BASE_ADDRESS)
+assert result_comments == True, "Ошибка при добавлении комментария"
+
+driver.close()
+driver.quit()
